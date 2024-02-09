5e4b0e56d8968776002f847d04c980c8a3e3d063
ImranGreet <88135011+ImranGreet@users.noreply.github.com>
2024-02-09 12:36:18 +0600

    how it works is done

--- src/Pages/HowWorks.vue
+++ src/Pages/HowWorks.vue
@@ -3,17 +3,20 @@
     <how />
     <Formula />
     <Procedure />
+    <Expectations />
   </section>
 </template>
 
 <script>
+import ExpectParent from "../components/HowItWorks/ExpectParent.vue";
+import Expectations from "../components/HowItWorks/Expectations.vue";
 import Formula from "../components/HowItWorks/Formula.vue";
 import How from "../components/HowItWorks/How.vue";
 import Procedure from "../components/HowItWorks/Procedure.vue";
 
 export default {
   name: "How Works",
-  components: { How, Formula, Procedure },
+  components: { How, Formula, Procedure, Expectations, ExpectParent },
 };
 </script>
 
